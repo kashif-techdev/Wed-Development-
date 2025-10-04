8783beb90b4cc9c4a391605f835c02236413e07a
Kashif <kashif.techdev@gmail.com>
2025-10-04 17:22:21 +0500

    skipping the first argument

---
+++ skipFirst.js
@@ -0,0 +1,21 @@
+function calculate(operation, ...nums)
+{
+  if (operation === "add") 
+    {
+    let sum = 0;
+    for (let n of nums) sum += n;
+    console.log("Addition:", sum);
+    }
+    else if (operation === "multiply") 
+    {
+    let product = 1;
+    for (let n of nums) product *= n;
+    console.log("Multiplication:", product);
+    } 
+    else {
+    console.log("Unknown operation!");
+  }
+}
+
+calculate("add", 2, 3, 4, 16);    
+calculate("multiply", 2, 17, 4);     
